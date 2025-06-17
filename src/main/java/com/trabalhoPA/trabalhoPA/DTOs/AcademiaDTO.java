2a3ba748b974f21c6bddfe10cf0b2ac22abe3cdd
Heloísa Jobbins Scheffer <101581054+helojobbinss@users.noreply.github.com>
2025-06-17 20:16:59 -0300

    Academia DTO

---
+++ src/main/java/com/trabalhoPA/trabalhoPA/DTOs/AcademiaDTO.java
@@ -0,0 +1,31 @@
+package com.trabalhoPA.trabalhoPA.DTOs;
+
+public class AcademiaDTO {
+    private String cnpj;
+    private String nome;
+    private String modalidade;
+    private String endereco;
+
+    // Construtores
+    public AcademiaDTO() {}
+
+    public AcademiaDTO(String cnpj, String nome, String modalidade, String endereco) {
+        this.cnpj = cnpj;
+        this.nome = nome;
+        this.modalidade = modalidade;
+        this.endereco = endereco;
+    }
+
+    // Getters e Setters
+    public String getCnpj() { return cnpj; }
+    public void setCnpj(String cnpj) { this.cnpj = cnpj; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getModalidade() { return modalidade; }
+    public void setModalidade(String modalidade) { this.modalidade = modalidade; }
+
+    public String getEndereco() { return endereco; }
+    public void setEndereco(String endereco) { this.endereco = endereco; }
+}
